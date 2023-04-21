bebc6f67703c25e302ce84c95b7de8de63deb31a
GTedZ <Hellis_over@icloud.com>
2023-04-21 09:52:58 +0300

    added time difference and interval events

--- Times.js
+++ Times.js
@@ -1,9 +1,42 @@
+const { EventEmitter } = require('events');
+
 const SECOND = 1000,
-    MINUTE = 60 * SECOND;
+    MINUTE = 60 * SECOND,
+    HOUR = 60 * MINUTE,
+    DAY = 24 * HOUR,
+    MONTH = 30 * DAY,
+    YEAR = 365 * DAY;
 ;
+
+const timeIntervals = {
+    SECOND,
+    MINUTE,
+    HOUR,
+    DAY,
+    MONTH,
+    YEAR
+}
 
 
 class Times {
+
+    all_UTC_Events = ["UTC-SECOND", "UTC-MINUTE", "UTC-HOUR", "UTC-DAY", "UTC-MONTH", "UTC-YEAR"];
+
+    all_Events = [...this.all_UTC_Events];
+
+    running_timeouts = {};
+
+    event = new EventEmitter();
+
+    constructor() {
+
+        for (const UTC_event of this.all_UTC_Events) {
+            const [, timeInterval] = UTC_event.split('-');
+            this.running_timeouts[timeInterval] = null;
+        }
+
+        prepareEventListeners(this);
+    }
 
     /**
      * @returns { {datetime:String, date:String, time:String, ISO:String, UTC:String, timestamp:number } } timeObject
@@ -43,6 +76,65 @@ class Times {
         return { datetime: dateTimeString, date: dateString, time: timeString, ISO: ISOString, UTC: UTCString, timestamp };
     }
 
+    /**
+     * @param {"MINUTE"|"HOUR"|"DAY"|"MONTH"|"YEAR"|"SECOND"} timeInterval
+     * @param {number|undefined} timestamp
+     * @returns {number} millis_til_next_timeInterval
+     */
+    millis_til_next_UTC_interval(timeInterval, customTimestamp = undefined) {
+        let timestamp = Date.now();
+
+        if (typeof timeInterval !== 'string') throw new Error(`'timeInterval' must be of type 'String', received '${typeof timeInterval}'`);
+        if (typeof timeIntervals[timeInterval] === 'undefined') throw new Error(`time interval '${timeInterval}' is invalid, valid values are "${Object.keys(timeIntervals).join('", "')}"`);
+
+        if (typeof customTimestamp !== 'undefined') {
+            if (typeof customTimestamp !== 'number') throw new Error(`'customTimestamp' should be of type 'number', received '${typeof customTimestamp}'`);
+            if (customTimestamp != customTimestamp) throw new Error(`Received an invalid value '${customTimestamp}'`);
+            timestamp = customTimestamp;
+        }
+
+        const interval = timeIntervals[timeInterval];
+
+        const currentMills_after_latest_interval = timestamp % interval;
+
+        return interval - currentMills_after_latest_interval;
+    }
+
+}
+
+function prepareEventListeners(instance) {
+
+    instance.event.on('newListener', (event) => {
+        if (instance.all_UTC_Events.includes(event)) {
+
+            const [, timeInterval] = event.split('-');
+            if (instance.event.listenerCount(event) == 0) start_UTC_eventEmitter(instance, timeInterval)
+
+        }
+    })
+
+    instance.event.on('removeListener', (event) => {
+        if (instance.all_UTC_Events.includes(event)) {
+
+            const [, timeInterval] = event.split('-');
+            if (instance.event.listenerCount(event) == 0) remove_UTC_eventEmitter(instance, timeInterval)
+
+        }
+    })
+}
+
+function start_UTC_eventEmitter(instance, interval) {
+    instance.running_timeouts[interval] = setTimeout(
+        () => {
+            instance.event.emit(`UTC-${interval}`);
+            start_UTC_eventEmitter(instance, interval);
+        },
+        instance.millis_til_next_UTC_interval(interval)
+    )
+}
+
+function remove_UTC_eventEmitter(instance, interval) {
+    clearTimeout(instance.running_timeouts[interval]);
 }
 
 const instance = new Times();
--- test.js
+++ test.js
@@ -1,6 +1,21 @@
 const time = require('./Times');
 
 const UTCTime = time.getUTCTime();
+
 const localTime = time.getLocalTime();
 
-console.log({ UTCTime, localTime })
+const millis_til_next_UTC_HOUR = time.millis_til_next_UTC_interval('HOUR');
+
+const millis_til_next_UTC_HOUR_from_specific_timestamp = time.millis_til_next_UTC_interval('HOUR', new Date('4/21/2023, 10:30 AM').getTime());
+
+time.event.on('UTC-MINUTE', () => {
+    // do something on every UTC minute
+})
+
+time.event.on('UTC-HOUR', () => {
+    // do something on every UTC Hour
+})
+
+time.event.on('UTC-DAY', () => {
+    // do something on every UTC Day
+})
